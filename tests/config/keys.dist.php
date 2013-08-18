b55f3ea2cdea7bf30a149ed446f8b19c2c494d06
Jerry Ablan <jerryablan@gmail.com>
2013-08-17 21:08:06 -0400

    Test keys removed.

---
+++ tests/config/keys.dist.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * keys.dist.php
+ *
+ * Copy this file to keys.php and place your private auth keys in here for testing.
+ */
+
+/**
+ * Facebook keys
+ */
+const FACEBOOK_CLIENT_ID = 'client_id';
+const FACEBOOK_CLIENT_SECRET = 'client_secret';
+
+/**
+ * Github keys
+ */
+const GITHUB_CLIENT_ID = 'client_id';
+const GITHUB_CLIENT_SECRET = 'client_secret';
+
+/**
+ * Twitter keys
+ */
+const TWITTER_CONSUMER_KEY = 'consummr_id';
+const TWITTER_CONSUMER_SECRET = 'consumer_secret';
--- tests/web/index.php
+++ tests/web/index.php
@@ -1,66 +1,55 @@
 <?php
-require_once dirname( dirname( __DIR__ ) ) . '/vendor/autoload.php';
+require_once dirname( __DIR__ ) . '/bootstrap.php';
 
 use DreamFactory\Oasys\Components\GateKeeper;
 use DreamFactory\Oasys\Components\GenericUser;
 use DreamFactory\Oasys\Components\OAuth\Enums\Flows;
+use DreamFactory\Oasys\Exceptions\OasysException;
 use DreamFactory\Oasys\Stores\FileSystem;
 use Kisma\Core\Utility\Convert;
 use Kisma\Core\Utility\Log;
 
 Log::setDefaultLog( __DIR__ . '/../log/error.log' );
 
-$_config = array();
+$_config = null;
 
-if ( file_exists( dirname( __DIR__ ) . '/config/oasys.config.php' ) )
+//	Choose the provider to test
+//$_providerId = 'facebook';
+//$_providerId = 'github';
+$_providerId = 'twitter';
+
+switch ( $_providerId )
 {
-	$_config = require( dirname( __DIR__ ) . '/config/oasys.config.php' );
+	case 'facebook':
+		$_config = array(
+			'flow_type'     => Flows::CLIENT_SIDE,
+			'client_id'     => FACEBOOK_CLIENT_ID,
+			'client_secret' => FACEBOOK_CLIENT_SECRET,
+		);
+		break;
+	case 'github':
+		$_config = array(
+			'flow_type'     => Flows::CLIENT_SIDE,
+			'client_id'     => GITHUB_CLIENT_ID,
+			'client_secret' => GITHUB_CLIENT_SECRET,
+		);
+		break;
+
+	case 'twitter':
+		array(
+			'consumer_key'    => TWITTER_CONSUMER_KEY,
+			'consumer_secret' => TWITTER_CONSUMER_SECRET,
+		);
+		break;
+
+	default:
+		throw new OasysException( 'Provider "' . $_providerId . '" not set supported here.' );
+		break;
 }
 
-$_gk = new GateKeeper(
-	array_merge(
-		array(
-			 'store' => new FileSystem( __FILE__ )
-		),
-		$_config
-	)
-);
-
-//	Facebook
-//$_provider = $_gk->getProvider(
-//	'facebook',
-//	array(
-//		 'flow_type'     => Flows::CLIENT_SIDE,
-//		 'client_id'     => '1392217090991437',
-//		 'client_secret' => 'd5dd3a24b1ec6c5f204a300ed24c60d0',
-//	)
-//);
-//
-//if ( $_provider->handleRequest() )
-//{
-//	header( 'Content-Type: application/json' );
-//	echo json_encode( $_provider->fetch( '/me' ) );
-//}
-
-//	Github
-//$_provider = $_gk->getProvider(
-//	'github',
-//	array(
-//		 'flow_type'     => Flows::CLIENT_SIDE,
-//		 'client_id'     => 'caf2ba694afc90d62c2a',
-//		 'client_secret' => '8f5b38a65ddfc0761febe0c113a2e128c43bac9e',
-//	)
-//);
-
-//	Twitter
-
-$_provider = $_gk->getProvider(
-	'twitter',
-	array(
-		 'consumer_key'    => 'slPaWdUIv2TYsgywu1pJ6w',
-		 'consumer_secret' => 'w9GP5LibncziUUoISao7itVs1FxD9vEZb8BwCVSN4',
-	)
-);
+//	Start it up
+$_oasys = new GateKeeper( array( 'store' => new FileSystem( __FILE__ ) ) );
+$_provider = $_oasys->getProvider( $_providerId, $_config );
 
 if ( $_provider->handleRequest() )
 {
