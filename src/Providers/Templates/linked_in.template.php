94e98a583bb9c2618bc3acd9038316436b9e3abe
Jerry Ablan <jerryablan@dreamfactory.com>
2014-12-05 10:50:30 -0500

    Current work checkpoint. See diff for details.

--- src/Providers/Templates/linked_in.template.php
+++ src/Providers/Templates/linked_in.template.php
@@ -9,7 +9,6 @@ namespace DreamFactory\Oasys\Providers\Templates;
 use DreamFactory\Oasys\Enums\EndpointTypes;
 use DreamFactory\Oasys\Enums\ProviderConfigTypes;
 use DreamFactory\Oasys\Enums\TokenTypes;
-use DreamFactory\Oasys\Providers\LinkedIn;
 
 /**
  * linked_in.template.php
@@ -20,18 +19,19 @@ use DreamFactory\Oasys\Providers\LinkedIn;
  */
 
 return array(
-	'id'                 => 'linked_in',
-	'type'               => ProviderConfigTypes::OAUTH,
-	'access_token_type'  => TokenTypes::BEARER,
-	'client_id'          => '{{client_id}}',
-	'client_secret'      => '{{client_secret}}',
-	'scope'              => LinkedIn::DEFAULT_SCOPE,
-	'redirect_proxy_url' => 'https://oasys.cloud.dreamfactory.com/oauth/authorize',
-	'endpoint_map'       => array(
+	'id'                      => 'linked_in',
+	'type'                    => ProviderConfigTypes::OAUTH,
+	'access_token_type'       => TokenTypes::URI,
+	'access_token_param_name' => 'oauth2_access_token',
+	'client_id'               => '{{client_id}}',
+	'client_secret'           => '{{client_secret}}',
+	'scope'                   => 'r_basicprofile r_emailaddress r_contactinfo',
+	'redirect_proxy_url'      => 'https://oasys.cloud.dreamfactory.com/oauth/authorize',
+	'endpoint_map'            => array(
 		EndpointTypes::AUTHORIZE    => 'https://www.linkedin.com/uas/oauth2/authorization',
 		EndpointTypes::ACCESS_TOKEN => 'https://www.linkedin.com/uas/oauth2/accessToken',
 		EndpointTypes::SERVICE      => 'https://api.linkedin.com/v1',
 		EndpointTypes::IDENTITY     => '/people/~',
 	),
-	'referrer_domain'    => 'facebook.com',
+	'referrer_domain'         => 'facebook.com',
 );
